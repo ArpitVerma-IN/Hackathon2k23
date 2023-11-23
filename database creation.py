5be9d89b7a643f405f7f1f1bef66fa6782f6a111
SatyamKumarn14 <satyamkumarn14@gmail.com>
2023-11-23 20:06:29 +0530

    Update database creation.py

--- database creation.py
+++ database creation.py
@@ -2,5 +2,5 @@ import mysql.connector as mql
 mycon=mql.connect(host='localhost',user='root',passwd='1234')
 cursor=mycon.cursor()
 mycon.autocommit=True
-s1="create database IF NOT EXISTS MDCLRCD"
+s1="create database IF NOT EXISTS MDCL_RCD"
 cursor.execute(s1)
