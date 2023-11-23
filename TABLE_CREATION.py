62362050e6824eb842a12c77c449af68e4625627
SatyamKumarn14 <satyamkumarn14@gmail.com>
2023-11-24 02:43:19 +0530

    Update TABLE_CREATION.py

--- TABLE_CREATION.py
+++ TABLE_CREATION.py
@@ -1,11 +1,11 @@
-def table_creation_NAME_PWD():
+'''def table_creation_NAME_PWD():
     import mysql.connector
     mycon=mysql.connector.connect(host='localhost',user='root',passwd='1234',database='mdcl_rcd')
     cursor=mycon.cursor()
     mycon.autocommit=True
     s1="create table IF NOT EXISTS md_id(MD_ID int(6),Name varchar(20),PASSWORD int(4))"
     cursor.execute(s1)
-table_creation_NAME_PWD()
+table_creation_NAME_PWD()'''
 
 
 def table_creation_BASIC_INFO():
@@ -13,7 +13,7 @@ def table_creation_BASIC_INFO():
     mycon=mysql.connector.connect(host='localhost',user='root',passwd='1234',database='mdcl_rcd')
     cursor=mycon.cursor()
     mycon.autocommit=True
-    s2="create table IF NOT EXISTS basic_info(MD_ID int(6),Name varchar(20),Age int(3),Sex varchar(1), Blood_Group varchar(3))"
+    s2="create table IF NOT EXISTS basic_info(MD_ID int(6),PASSWORD int(4),Name varchar(20),Age int(3),Sex varchar(1), Blood_Group varchar(3))"
     cursor.execute(s2)
 table_creation_BASIC_INFO()
 
