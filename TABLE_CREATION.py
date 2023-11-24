656b8e12a9a2b6927ea422d5d42188f63ea7e1b7
SatyamKumarn14 <satyamkumarn14@gmail.com>
2023-11-24 10:10:50 +0530

    Update TABLE_CREATION.py

--- TABLE_CREATION.py
+++ TABLE_CREATION.py
@@ -23,6 +23,6 @@ def table_creation_MED_INFO():
     mycon=mysql.connector.connect(host='localhost',user='root',passwd='1234',database='mdcl_rcd')
     cursor=mycon.cursor()
     mycon.autocommit=True
-    s3="create table IF NOT EXISTS MED_INFO(DISEASES varchar(100), ALLERGIES varchar(100), GENETIC_INFO varchar(100),SURGICAL_HISTORY varchar(100),OTHER varchar(100))"
+    s3="create table IF NOT EXISTS MED_INFO(MD_ID int(6),DISEASES varchar(100), ALLERGIES varchar(100), GENETIC_INFO varchar(100),SURGICAL_HISTORY varchar(100),OTHER varchar(100))"
     cursor.execute(s3)
 table_creation_MED_INFO()
