0d71180db71c44519a28f2699b715cec0b69c9fd
SatyamKumarn14 <satyamkumarn14@gmail.com>
2023-11-24 15:02:13 +0530

    Update TABLE_CREATION.py

--- TABLE_CREATION.py
+++ TABLE_CREATION.py
@@ -13,7 +13,7 @@ def table_creation_BASIC_INFO():
     mycon=mysql.connector.connect(host='localhost',user='root',passwd='1234',database='mdcl_rcd')
     cursor=mycon.cursor()
     mycon.autocommit=True
-    s2="create table IF NOT EXISTS basic_info(MD_ID int(6),PASSWORD int(4),Name varchar(20),Age int(3),Sex varchar(1), Blood_Group varchar(3))"
+    s2="create table IF NOT EXISTS basic_info(MD_ID int(6)PRIMARY KEY,PASSWORD int(4),Name varchar(20),Age int(3),Sex varchar(1), Blood_Group varchar(3))"
     cursor.execute(s2)
 table_creation_BASIC_INFO()
 
@@ -23,6 +23,6 @@ def table_creation_MED_INFO():
     mycon=mysql.connector.connect(host='localhost',user='root',passwd='1234',database='mdcl_rcd')
     cursor=mycon.cursor()
     mycon.autocommit=True
-    s3="create table IF NOT EXISTS MED_INFO(MD_ID int(6),DISEASES varchar(100), ALLERGIES varchar(100), GENETIC_INFO varchar(100),SURGICAL_HISTORY varchar(100),OTHER varchar(100))"
+    s3="create table IF NOT EXISTS MED_INFO(MD_ID int(6)PRIMARY KEY,DISEASES varchar(100), ALLERGIES varchar(100), GENETIC_INFO varchar(100),SURGICAL_HISTORY varchar(100),OTHER varchar(100))"
     cursor.execute(s3)
 table_creation_MED_INFO()
